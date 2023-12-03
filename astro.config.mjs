3505fba841fbae15279cb1e74a239670b9e3f85d
natgo <xenhud@protonmail.com>
2023-12-03 03:19:20 +0200

    Move to bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react({ include: ["**/react/*"] }),
-    solidJs({ include: ["**/solid/*"] }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react({
+    include: ["**/react/*"]
+  }), solidJs({
+    include: ["**/solid/*"]
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
